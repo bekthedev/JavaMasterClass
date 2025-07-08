05010633b54e84ddf067c4fa12f6a93221c9e3bc
Bek Johansson <beksaylor@gmail.com>
2025-07-07 21:46:14 -0500

    update file

---
+++ src/CodeBlock/Challenge.java
@@ -0,0 +1,23 @@
+package CodeBlock;
+
+public class Challenge {
+    public static void main(String[] args) {
+        boolean gameOver = true;
+
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        int finalScore = score;
+
+        if (gameOver) {
+            finalScore += (bonus * levelCompleted);
+            System.out.println("Your final score was " + finalScore);
+        }
+    }
+}
---
+++ src/CodeBlock/Main.java
@@ -0,0 +1,21 @@
+package CodeBlock;
+
+public class Main {
+    public static void main(String[] args) {
+
+        boolean gameOver = true;
+        int score = 4000;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if (score < 5000 && score > 1000) {
+            System.out.println("Your score is was less than 5000 but greater than 1000");
+        } else if (score < 1000) {
+            System.out.println("Your score is less than 1000");
+        } else {
+            System.out.println("Got here");
+        }
+    }
+
+
+}
