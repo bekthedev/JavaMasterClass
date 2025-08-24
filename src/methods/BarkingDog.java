1c3940df8dddb24ff35b82fc6144e3dbf11007b1
Bek Johansson <beksaylor@gmail.com>
2025-08-24 00:40:52 -0500

    Barking Dog

---
+++ src/methods/BarkingDog.java
@@ -0,0 +1,25 @@
+package methods;
+
+public class BarkingDog {
+    public static void main(String[] args) {
+
+        System.out.println(shouldWakeUp(true, 1));
+        System.out.println(shouldWakeUp(true, 8));
+        System.out.println(shouldWakeUp(false, 2));
+        System.out.println(shouldWakeUp(true, -8));
+    }
+
+    public static boolean shouldWakeUp(boolean barking,int hourOfDay) {
+
+        if (hourOfDay < 0 || hourOfDay > 23) {
+            return false;
+        }
+
+        if ((hourOfDay < 8 || hourOfDay > 22) && barking) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
