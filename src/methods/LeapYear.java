4f26c375635bb91338908aeb107a07dad1e7e92f
Bek Johansson <beksaylor@gmail.com>
2025-08-24 01:07:32 -0500

    LeapYear

---
+++ src/methods/LeapYear.java
@@ -0,0 +1,23 @@
+package methods;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        System.out.println(isLeapYear(-1600));
+        System.out.println(isLeapYear(1600));
+        System.out.println(isLeapYear(2017));
+        System.out.println(isLeapYear(2000));
+        System.out.println(isLeapYear(1972));
+    }
+
+    public static boolean isLeapYear(int year) {
+        if(year < 1 || year > 9999) {
+            return false;
+        }
+
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
