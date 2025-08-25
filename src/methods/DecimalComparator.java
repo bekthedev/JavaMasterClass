cf4d3e7a91f6b4e9881b750c2aeedd8c8aacf7f8
Bek Johansson <beksaylor@gmail.com>
2025-08-25 07:17:25 -0500

    add:DecimalComparator

---
+++ src/methods/DecimalComparator.java
@@ -0,0 +1,17 @@
+package methods;
+
+public class DecimalComparator {
+    public static void main(String[] args) {
+        System.out.println(areEqualByThreeDecimalPlaces(-3.1756, -3.175)); // true
+        System.out.println(areEqualByThreeDecimalPlaces(3.175, 3.176));    // false
+        System.out.println(areEqualByThreeDecimalPlaces(3.0, 3.0));        // true
+        System.out.println(areEqualByThreeDecimalPlaces(-3.123, 3.123));   // false
+    }
+
+    public static boolean areEqualByThreeDecimalPlaces(double x, double y) {
+        int a = (int)(x * 1000);
+        int b = (int)(y * 1000);
+
+        return a == b;
+    }
+}
