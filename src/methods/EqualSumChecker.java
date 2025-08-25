3c41374dac18b17f2c355da3d9881235fda5680c
Bek Johansson <beksaylor@gmail.com>
2025-08-25 07:25:09 -0500

    add:EqualSumChecker

---
+++ src/methods/EqualSumChecker.java
@@ -0,0 +1,16 @@
+package methods;
+
+public class EqualSumChecker {
+    public static void main(String[] args) {
+
+        System.out.println(hasEqualSum(1, 1, 1));  // false
+        System.out.println(hasEqualSum(1, 1, 2));  // true
+        System.out.println(hasEqualSum(1, -1, 0)); // true
+
+    }
+
+    public static boolean hasEqualSum(int x, int y, int z) {
+
+        return (x + y == z);
+    }
+}
