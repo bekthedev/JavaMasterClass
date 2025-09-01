d8111967b6ec02435898b33a502df16ea5186881
Bek Johansson <beksaylor@gmail.com>
2025-08-31 23:37:33 -0500

    add:validation

--- src/methods/overloading/SecondsAndMinutesChallenge.java
+++ src/methods/overloading/SecondsAndMinutesChallenge.java
@@ -3,15 +3,29 @@ package methods.overloading;
 public class SecondsAndMinutesChallenge {
     public static void main(String[] args) {
 
-        System.out.println(getDurationString(3945));
+        System.out.println(getDurationString(-3945));
+        System.out.println(getDurationString(-65, 45));
+        System.out.println(getDurationString(65, 145));
         System.out.println(getDurationString(65, 45));
     }
 
     public static String getDurationString(int seconds) {
+        if (seconds < 0) {
+            return "Seconds cannot be negative!";
+        }
+
         int minutes = seconds / 60;
-       return getDurationString(minutes, seconds);
+        return getDurationString(minutes, seconds);
     }
     public static String getDurationString(int minutes, int seconds) {
+
+        if (minutes < 0) {
+            return "Minutes cannot be negative!";
+        }
+
+        if (seconds < 0 || seconds > 59) {
+            return "Invalid seconds, must be between 0 and 59 inclusive!";
+        }
 
         int hours = minutes / 60;
 
